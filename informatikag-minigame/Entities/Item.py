774a8be7561ae9c0a194a8de31ca47676ee576d8
Dürhager <robin.duerhager@hshl.de>
2025-09-29 13:05:54 +0200

    refactor item module

--- informatikag-minigame/Entities/Item.py
+++ informatikag-minigame/Entities/Item.py
@@ -1,18 +1,18 @@
 import random
 from enum import Enum
 
-item_type = Enum('item_type', [('DEFAULT', 1), ('WEAPON', 2), ('ARMOUR', 3), ('CONSUMABLE', 4)])
+item_type = Enum('item_type', [('DEFAULT', 1), ('WEAPON', 2)])
 weapon_type = Enum('weapon_type', [('FISTS', 1), ('BLADE', 2), ('BOW', 3)])
 
 _items = {
     'WEAPONS': [
-        {'type': item_type.DEFAULT, 'weapon_type': weapon_type.FISTS, 'name': 'Fists', 'damage_multiplier': 1},
         # Default weapon is always on index 0
+        {'type': item_type.DEFAULT, 'weapon_type': weapon_type.FISTS, 'name': 'Fists', 'damage_multiplier': 1},
         {'type': item_type.WEAPON, 'weapon_type': weapon_type.BLADE, 'name': 'blade_test', 'damage_multiplier': 2},
         {'type': item_type.WEAPON, 'weapon_type': weapon_type.BOW, 'name': 'bow_test', 'damage_multiplier': 3}
     ],
-    'ARMOUR': [],
-    'CONSUMABLES': [],
+    #'ARMOUR': [], #TODO: Implement more Item Types
+    #'CONSUMABLES': [],
 }
 
 
